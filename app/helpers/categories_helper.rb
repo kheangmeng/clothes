bb71f51338ddbdaec82f4e0f3108971b0ac47331
kheangmengte <kheangmengte@gmail.com>
2016-09-02 12:07:30 +0700

    Add help and User layout

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,17 @@
 module CategoriesHelper
+	def check_stock(pro)
+		if pro.quentity == 0
+			content_tag(:strong, "Out off stock")
+		else
+			pro.quentity
+		end
+	end
+
+	def check_discount(pro)
+		if pro.discount == 0
+			number_to_currency(pro.discount)
+		else
+			content_tag(:strong, number_to_currency(pro.discount))
+		end
+	end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,17 @@
 module ProductsHelper
+	def check_stock(product)
+		if product.quentity == 0
+			content_tag(:strong, "Out off stock")
+		else
+			product.quentity
+		end
+	end
+
+	def check_discount(product)
+		if product.discount == 0
+			number_to_currency(product.discount)
+		else
+			content_tag(:strong, number_to_currency(product.discount))
+		end
+	end
 end
